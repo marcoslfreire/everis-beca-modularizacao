fd513e5e93bc2f9f81446f879b18b37904a0d124
Marcos Luciano <marcos3dt@gmail.com>
2021-04-11 15:40:13 -0300

    criando o pacote login

--- app/src/main/java/com/niemietz/everis/beca/modularizacao/login/interfaces/LoginAPI.kt
+++ app/src/main/java/com/niemietz/everis/beca/modularizacao/login/interfaces/LoginAPI.kt
@@ -1,7 +1,7 @@
 package com.niemietz.everis.beca.modularizacao.login.interfaces
 
-import com.niemietz.everis.beca.modularizacao.login.constants.APIConstants.AUTHENTICATE
-import com.niemietz.everis.beca.modularizacao.login.constants.APIConstants.GET_SESSION
+import com.example.login.constants.APIConstants.AUTHENTICATE
+import com.example.login.constants.APIConstants.GET_SESSION
 import com.niemietz.everis.beca.modularizacao.login.model.GETSessionResponse
 import com.niemietz.everis.beca.modularizacao.login.model.GETSessionRequest
 import com.niemietz.everis.beca.modularizacao.login.model.AuthenticateResponse
--- login/src/main/java/com/example/login/constants/APIConstants.kt
+++ login/src/main/java/com/example/login/constants/APIConstants.kt
@@ -1,4 +1,4 @@
-package com.niemietz.everis.beca.modularizacao.login.constants
+package com.example.login.constants
 
 object APIConstants {
     const val GET_SESSION = "login/session"
